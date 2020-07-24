8c20629cd55c9353dd4a4a9221a6b179ec333608
evrgreen <rcarsten06@gmail.com>
2020-07-24 09:14:24 -0600

    day 4 mvp complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,16 +3,24 @@
 
 import sys
 
-LDI = 0b10000010
-PRN = 0b01000111
+CALL = 0b01010000
 HLT = 0b00000001
+IRET = 0b00010011
+JMP = 0b01010100
+LDI = 0b10000010
 POP = 0b01000110
+PRA = 0b01001000
+PRN = 0b01000111
 PUSH = 0b01000101
+RET = 0b00010001
+ST = 0b10000100
+# ALU ops
 MUL = 0b10100010
 ADD = 0b10100000
 DIV = 0b10100011
 SUB = 0b10100001
 SP = 7
+
 
 class CPU:
     """Main CPU class."""
@@ -22,52 +30,106 @@ class CPU:
         self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
+        self.fl = 0
         self.register[SP] = 0xF4
         self.process_table = {
-            LDI : self.ldi,
-            PRN : self.prn,
-            HLT : self.hlt,
-            POP : self.pop,
+            CALL: self.call,
+            HLT: self.hlt,
+            IRET: self.iret,
+            JMP: self.jmp,
+            LDI: self.ldi,
+            POP: self.pop,
+            PRA: self.pra,
+            PRN: self.prn,
             PUSH: self.push,
-            MUL : self.alu,
-            ADD : self.alu,
-            DIV : self.alu,
-            SUB : self.alu
+            RET: self.ret,
+            ST: self.st,
+            MUL: self.alu,
+            ADD: self.alu,
+            DIV: self.alu,
+            SUB: self.alu
         }
 
-    def ldi(self, op_a, op_b):
-        self.register[op_a] = op_b 
-    
-    def prn(self, op_a, op_b=None):
-        print(self.register[op_a])
+    def call(self, op_a, op_b=None):
+        '''
+        Calls a subroutine(function) at address stored in register[op_a]
+        '''
+        # Decriment SP
+        self.register[SP] -= 1
+        self.ram_write(self.register[SP], self.pc + 2)
+        self.pc = self.register[op_a]
 
-    def hlt(self, op_a, op_b=None):
+    def hlt(self, op_a=None, op_b=None):
+        '''
+        Halt cpu and exit emulator
+        '''
         sys.exit()
-    
+
+    def jmp(self, op_a, op_b=None):
+        '''
+        Jump to the address stored in register[op_a]
+        '''
+        self.pc = self.register[op_a]
+
+    def ldi(self, op_a, op_b):
+        '''
+        Set the value of register[op_a] to op_b
+        '''
+        self.register[op_a] = op_b
+
     def pop(self, op_a, op_b=None):
+        '''
+        Pop the value at the top of the stack into register[op_a]
+        '''
         value = self.ram_read(self.register[SP])
         self.register[op_a] = value
         self.register[SP] += 1
         return value
-    
+
+    def pra(self, op_a, op_b=None):
+        '''
+        Print alpha character value stored in the given register
+        '''
+        print(chr(self.register[op_a]))
+
+    def prn(self, op_a, op_b=None):
+        '''
+        Print numeric value stored in register[op_a]
+        '''
+        print(self.register[op_a])
+
     def push(self, op_a, op_b=None):
+        '''
+        Push the value in the register[op_a] onto the stack
+        '''
         self.register[SP] -= 1
         self.ram_write(self.register[SP], self.register[op_a])
-        
+
+    def ret(self, op_a=None, op_b=None):
+        '''
+        Return from subroutine
+        '''
+        self.pc = self.ram_read(self.register[SP])
+        self.register[SP] += 1
+
+    def st(self, op_a, op_b):
+        '''
+        Store value in register[op_b] in the address stored in register[op_a]
+        '''
+        self.ram_write(self.register[op_a], self.register[op_b])
+
     def load(self, filename):
         """Load a program into memory."""
-        address = 0
 
+        address = 0
         with open(filename) as f:
             for line in f:
                 line = line.split('#')
                 line = line[0].strip()
                 if line == '':
                     continue
-                self.ram[address] = int(line, 2)      
+                self.ram[address] = int(line, 2)
                 address += 1
-
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -79,7 +141,10 @@ class CPU:
         elif op == MUL:
             self.register[reg_a] *= self.register[reg_b]
         elif op == DIV:
-            self.register[reg_a] /= self.register[reg_b]
+            if self.register[reg_b] != 0:
+                self.register[reg_a] /= self.register[reg_b]
+            else:
+                raise Exception("Cannot divide by 0")
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -89,7 +154,7 @@ class CPU:
         '''
         mdr = self.ram[mar]
         return mdr
-    
+
     def ram_write(self, mar, mdr):
         '''
         Stores 'value' (MDR) at given 'address' (MAR) in ram
@@ -100,12 +165,14 @@ class CPU:
         '''
         Run the CPU
         '''
+
         while True:
             ir = self.ram[self.pc]
             op_a = self.ram_read(self.pc + 1)
             op_b = self.ram_read(self.pc + 2)
             run_counter = (ir >> 6) + 1
-            alu_op = bool(((ir >> 5) & 0b1))  
+            alu_op = bool(((ir >> 5) & 0b1))
+            set_pc = ((ir >> 4) & 0b1)
             if ir in self.process_table:
                 if alu_op:
                     self.process_table[ir](ir, op_a, op_b)
@@ -113,8 +180,8 @@ class CPU:
                     self.process_table[ir](op_a, op_b)
             else:
                 print('Unsupported operation')
-            self.pc += run_counter
-    
+            if not set_pc:
+                self.pc += run_counter
 
     def trace(self):
         """
@@ -124,8 +191,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -135,4 +202,3 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-
